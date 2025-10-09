2bc261accf07cc50a5c8cf62e2f46b98b23477c8
Titley <Joshua.Titley@tarmac.com>
2025-10-09 11:36:25 +0100

    Added a new check balance class to display the current balance

---
+++ CheckBalance.java
@@ -0,0 +1,8 @@
+package FinCore;
+
+public class CheckBalance {
+    // Prints the current balance saved
+    public void displayCurrentBalance(double currentBalance) {
+        IO.println("Your current balance is: $" + currentBalance);
+    }
+}
